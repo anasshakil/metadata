5f61c066540bd573a7a9faa38bae8ba247ac99de
Anas Shakil <35932749+anasshakil@users.noreply.github.com>
2023-02-28 23:54:23 +0500

    ci revert and shell file PWD fixed

--- .github/exiftool.sh
+++ .github/exiftool.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Author : Ahmed Anas Shakil
+cd "$(dirname "$0")"
+# cd "${0%/*}"
 cd ../
-mkdir .exiftool
+mkdir -p .exiftool
 cd .exiftool/
-wget -c https://liquidtelecom.dl.sourceforge.net/project/exiftool/Image-ExifTool-12.57.tar.gz -O - | tar -xz --strip-components=1
+wget -q -O - https://liquidtelecom.dl.sourceforge.net/project/exiftool/Image-ExifTool-12.57.tar.gz | tar -zx --exclude='html' --strip-components=1
